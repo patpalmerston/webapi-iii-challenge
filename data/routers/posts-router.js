afa52dc0d0d9a19958a813ff0135863838275f50
patpalmerston <patpalmerston@gmail.com>
2019-05-01 15:53:23 -0700

    delete functional on posts

--- data/routers/posts-router.js
+++ data/routers/posts-router.js
@@ -26,36 +26,50 @@ router.get('/:id', async (req, res) => {
   } catch (err) {sendUserError(500, "The post information could not be retrieved.", err )}
 });
 
-// router.post('/', async (req, res) => {
-//   const post = req.body;
-//   if (post.text && post.user_id) {
-//     postDb.insert(post)
-//       .then(post => {
-//         res.status(200).json(post)
-//       })
-//       .catch(err => {
-//         res.status(500).json({
-//           message: 'Error retrieving the posts'
-//         });
-//       });
-//   } else {
-//     res.status(400).json({
-//       errorMessage: "There was an error while saving the post to the database."
+router.post('/:id', async (req, res) => {
+  const post = req.body;
+  if (post.text && post.user_id) {
+    postDb.insert(post)
+      .then(post => {
+        res.status(200).json(post)
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'Error retrieving the posts'
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "There was an error while saving the post to the database."
+    });
+  }
+});
+
+// diff post version to insert new post
+
+
+// router.post('/:id', async (req, res) => {
+//   try {
+//     const myPost = await postDb.insert(req.body);
+//     res.status(201).json(myPost);
+//   } catch (error) {
+//     // log error to database
+//     console.log(error);
+//     res.status(500).json({
+//       message: 'Error adding post',
 //     });
 //   }
 // });
 
-router.post('/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
-    const myPost = await postDb.insert(req.body);
-    res.status(201).json(myPost);
-  } catch (error) {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-      message: 'Error adding post',
-    });
-  }
+    const post = await postDb.remove(req.params.id);
+    if (post > 0) {
+      res.status(200).json({ message: 'The Post has been Destroyed!'})
+    } else {
+      res.status().json({ message: "the post with that ID can not be found"})
+    }
+  } catch (err) {sendUserError(500, "the post could not be removed", err)}
 });
 
 module.exports = router;
