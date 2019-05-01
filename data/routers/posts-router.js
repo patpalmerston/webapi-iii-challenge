523d02bf6d2a044c2dfbba336da183cfe0eb7579
patpalmerston <patpalmerston@gmail.com>
2019-05-01 15:45:03 -0700

    insert running

--- data/routers/posts-router.js
+++ data/routers/posts-router.js
@@ -7,13 +7,55 @@ const sendUserError = (status, message, res) => {
   return;
 }
 
-
-
 router.get('/', async (req, res) => {
   try {
     const posts = await postDb.get();
     res.status(200).json(posts)
   } catch (err) {sendUserError(500, 'The post with the specified ID does not exist.', err)}
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await postDb.getById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ error: "The post information could not be retrieved." })
+    }
+  } catch (err) {sendUserError(500, "The post information could not be retrieved.", err )}
+});
+
+// router.post('/', async (req, res) => {
+//   const post = req.body;
+//   if (post.text && post.user_id) {
+//     postDb.insert(post)
+//       .then(post => {
+//         res.status(200).json(post)
+//       })
+//       .catch(err => {
+//         res.status(500).json({
+//           message: 'Error retrieving the posts'
+//         });
+//       });
+//   } else {
+//     res.status(400).json({
+//       errorMessage: "There was an error while saving the post to the database."
+//     });
+//   }
+// });
+
+router.post('/:id', async (req, res) => {
+  try {
+    const myPost = await postDb.insert(req.body);
+    res.status(201).json(myPost);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding post',
+    });
+  }
 });
 
 module.exports = router;
