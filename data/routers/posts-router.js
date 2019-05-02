4875a2a03ad30fb7c13876c3d551219a43634619
patpalmerston <patpalmerston@gmail.com>
2019-05-01 17:26:41 -0700

    post and put and delete all active on both routers

--- data/routers/posts-router.js
+++ data/routers/posts-router.js
@@ -26,40 +26,40 @@ router.get('/:id', async (req, res) => {
   } catch (err) {sendUserError(500, "The post information could not be retrieved.", err )}
 });
 
-router.post('/:id', async (req, res) => {
-  const post = req.body;
-  if (post.text && post.user_id) {
-    postDb.insert(post)
-      .then(post => {
-        res.status(200).json(post)
-      })
-      .catch(err => {
-        res.status(500).json({
-          message: 'Error retrieving the posts'
-        });
-      });
-  } else {
-    res.status(400).json({
-      errorMessage: "There was an error while saving the post to the database."
-    });
-  }
-});
+// router.post('/:id', async (req, res) => {
+//   const post = req.body;
+//   if (post.text && post.user_id) {
+//     postDb.insert(post)
+//       .then(post => {
+//         res.status(200).json(post)
+//       })
+//       .catch(err => {
+//         res.status(500).json({
+//           message: 'Error retrieving the posts'
+//         });
+//       });
+//   } else {
+//     res.status(400).json({
+//       errorMessage: "There was an error while saving the post to the database."
+//     });
+//   }
+// });
 
 // diff post version to insert new post
 
 
-// router.post('/:id', async (req, res) => {
-//   try {
-//     const myPost = await postDb.insert(req.body);
-//     res.status(201).json(myPost);
-//   } catch (error) {
-//     // log error to database
-//     console.log(error);
-//     res.status(500).json({
-//       message: 'Error adding post',
-//     });
-//   }
-// });
+router.post('/', async (req, res) => {
+  try {
+    const myPost = await postDb.insert(req.body);
+    res.status(201).json(myPost);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding post',
+    });
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   try {
@@ -71,5 +71,57 @@ router.delete('/:id', async (req, res) => {
     }
   } catch (err) {sendUserError(500, "the post could not be removed", err)}
 });
+
+// router.put('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const { text } = req.body;
+//   if (!text) {
+//     sendUserError(400, 'Must provide title and contents', res);
+//     return;
+//   }
+//   postDb
+//     .insert(id, { text })
+//     .then(response => {
+//       if (response == 0) {
+//         sendUserError(
+//           404,
+//           'The user with the specified ID does not exist.',
+//           res
+//         );
+//         return;
+//       }
+//       postDb
+//         .getById(id)
+//         .then(post => {
+//           if (post.length === 0) {
+//             sendUserError(404, 'User with that id not found', res);
+//             return;
+//           }
+//           res.json(post);
+//         })
+//         .catch(error => {
+//           sendUserError(500, 'Error looking up user', res);
+//         });
+//     })
+//     .catch(error => {
+//       sendUserError(500, 'Something bad happened in the database', res);
+//       return;
+//     });
+// });
+router.put('/:id', async (req, res) => {
+  try{
+    const post = await postDb.update(req.params.id, req.body);
+    if (post) {
+      res.status(200).json(post)
+    } else {
+      res.status(404).json({ message: 'the post could not be found'})
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error updating the post!'
+    })
+  }
+});
+
 
 module.exports = router;
--- data/routers/users-router.js
+++ data/routers/users-router.js
@@ -14,4 +14,55 @@ router.get('/', async (req, res) => {
   } catch (err) {sendUserError(500, 'The post with the specified ID does not exist.', err)}
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await userDb.getById(req.params.id);
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ error: "The post information could not be retrieved." })
+    }
+  } catch (err) {sendUserError(500, "The post information could not be retrieved.", err )}
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const myUser = await userDb.insert(req.body);
+    res.status(201).json(myUser);
+  } catch (error) {
+    // log error to database
+    console.log(error);
+    res.status(500).json({
+      message: 'Error adding post',
+    });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await userDb.remove(req.params.id);
+    if (user > 0) {
+      res.status(200).json({ message: 'The user has been Destroyed!'})
+    } else {
+      res.status().json({ message: "The user with that ID can not be found"})
+    }
+  } catch (err) {sendUserError(500, "The user could not be removed", err)}
+});
+
+router.put('/:id', async (req, res) => {
+  try{
+    const user = await userDb.update(req.params.id, req.body);
+    if (user) {
+      res.status(200).json(user)
+    } else {
+      res.status(404).json({ message: 'the post could not be found'})
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error updating the post!'
+    })
+  }
+});
+
 module.exports = router;
